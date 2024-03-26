2afc3e37d0f76e4d34e696a542cd77ce8813ba63
seynax <seyronax@gmail.com>
2024-03-26 11:20:50 +0100

    feature(new-animal): griffin and dragon animals

---
+++ cesiAnimal/cesiAnimal/animal/Griffin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesiAnimal.animal
+{
+    internal class Griffin : Animal
+    {
+        public Griffin()
+        {
+            this.name = "Griffin";
+        }
+
+        public override void cry()
+        {
+            Console.WriteLine("Anini !");
+        }
+
+        public new void sleep()
+        {
+            Console.WriteLine(this.name + " strange sleeping");
+        }
+    }
+}
---
+++ cesiAnimal/cesiAnimal/animal/Sheep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesiAnimal.animal
+{
+    internal class Sheep : Animal
+    {
+        public Sheep()
+        {
+            this.name = "Sheep";
+        }
+
+        public override void cry()
+        {
+            Console.WriteLine("Bee Bee !");
+        }
+    }
+}
