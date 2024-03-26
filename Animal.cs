3db9f68d0dd0d1c7415b40c8157b2bcf8b33f0e4
seynax <seyronax@gmail.com>
2024-03-26 09:36:45 +0100

    Animals initial code

---
+++ Animal.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class Animal
+{
+    public String name;
+
+    public Animal()
+    {
+
+    }
+
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+
+    public void move()
+    {
+        if(name == null)
+        {
+            Console.WriteLine("invisible animal move");
+
+            return;
+        }
+
+        Console.WriteLine(name + " move");
+    }
+
+    public void sleep()
+    {
+        if (name == null)
+        {
+            Console.WriteLine("invisible animal sleep");
+
+            return;
+        }
+
+        Console.WriteLine(name + " sleep");
+    }
+
+    public abstract void cry();
+
+    public class Wolf : Animal
+    {
+        public Wolf()
+        {
+            this.name = "Wolf";
+        }
+
+        public override void cry()
+        {
+            Console.WriteLine("Arou Arou !");
+        }
+
+        public void eat(Animal animal)
+        {
+            Console.WriteLine(this.name + " eat " + animal.name);
+        }
+    }
+
+    public class Sheep : Animal
+    {
+        public Sheep()
+        {
+            this.name = "Sheep";
+        }
+
+        public override void cry()
+        {
+            Console.WriteLine("Bee Bee !");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using static Animal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Wolf wolf = new Wolf();
+        Sheep sheep = new Sheep();
+
+        wolf.cry();
+        wolf.move();
+        sheep.move();
+        wolf.eat(sheep);
+        sheep.cry();
+        wolf.sleep();
+    }
+}
